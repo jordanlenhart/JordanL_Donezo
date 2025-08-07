fcaea34ffede758a533558ccc16c3f8e1a11b452
Jordan <jordinholenhart@gmail.com>
2025-08-06 20:45:41 -0700

    edited things in order to get the functionality working correctly. Fixed syntax

--- frontend/src/axios-instance.js
+++ frontend/src/axios-instance.js
@@ -2,10 +2,18 @@ import axios from "axios";
 import supabase from "./client";
 
 const getAxiosClient = async () => {
-  // Get the supabase session
-  // create an axios instance with the supabase access_token
-  // Return the instance
-  // Fill code here
+  const currentSession = await supabase.auth.getSession();
+  const { data } = await supabase.auth.getSession();
+  if (!data.session) throw new Error("No active session");
+  supabase.auth.getSession().then(console.log);
+
+  const instance = axios.create({
+    headers: {
+      Authorization: `Bearer ${currentSession.data.session.access_token}`,
+    },
+  });
+
+  return instance;
 };
 
 export default getAxiosClient;
